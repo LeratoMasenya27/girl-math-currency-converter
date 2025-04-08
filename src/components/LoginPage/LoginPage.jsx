278076a8bf469025debb88c533157129e0934e2c
Lerato Masenya <masenyalerato270@gmail.com>
2025-04-08 10:05:49 +0200

    Login Page html

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -3,7 +3,31 @@ import "../LoginPage/LoginPage.css";
 
 const LoginPage = () => {
     return (
-        <div>LoginPage</div>
+        <div className="wrapper">
+            <form action="">
+                <h1>Girl Math</h1>
+                <h2>Currency Converter</h2>
+                <h3>Email/ Username</h3>
+                <div className="input-box">
+                    <input type="text" placeholder="Email/ Username" required/>
+                </div>
+                <div className="input-box">
+                    <input type="password" placeholder="Password" required/>
+                </div>
+
+                <div className="remember-forgot">
+                    <label><input type="checkbox" />Remember me</label>
+                    <a href="#">Forgot password?</a>
+                </div>
+
+                <button type="submit">Login & Justify</button>
+
+                <div className="register-link">
+                    <p>New here? Create an <a href="#">account</a> and make math fun!</p>
+                </div>
+            </form>
+
+        </div>
     );
 };
 
